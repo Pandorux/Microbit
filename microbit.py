09d2092887c400d8b4b20e2debccfddf54ead1df
Jared Carey <itspandorux@gmail.com>
2018-11-04 11:56:35 +1000

    HappySad Script Created

---
+++ microbit.py
@@ -0,0 +1,15 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+
+while True:
+    if button_a.is_pressed():
+        display.show(Image.HAPPY)
+    elif button_b.is_pressed():
+        break
+    else:
+        display.show(Image.SAD)
+        
+display.clear()
+
+    
